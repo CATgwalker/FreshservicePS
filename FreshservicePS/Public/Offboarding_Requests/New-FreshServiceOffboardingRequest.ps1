846caeeb6bd45ad9c5222314277de3ab110d8c79
CATgwalker <78571293+CATgwalker@users.noreply.github.com>
2025-02-06 17:41:56 -0600

    Update New-FreshServiceOffboardingRequest.ps1

--- FreshservicePS/Public/Offboarding_Requests/New-FreshServiceOffboardingRequest.ps1
+++ FreshservicePS/Public/Offboarding_Requests/New-FreshServiceOffboardingRequest.ps1
@@ -6,14 +6,10 @@
     Creates Freshservice Offboarding Request via REST API.
     https://api.freshservice.com/#create_offboarding_request
 
-.PARAMETER EmployeeType
-    String value of the Employee Type.
-.PARAMETER FirstName
-    String value of the First Name.
-.PARAMETER LastName
-    String value of the Last Name.
-.PARAMETER StartDate
-    Datetime value of the Start Date
+.PARAMETER EmployeeEmail
+    String value of the Employee Email address.
+.PARAMETER ManagerEmail
+    String value of the Manager Email address.
 .PARAMETER OtherValues
     Hashtable containing any additional Offboarding Request custom fields.
 
@@ -24,7 +20,7 @@
         cf_product_code         = "1001"
     }
 
-    New-FreshServiceOffboardingRequest -FirstName $FirstName -LastName $LastName -EmployeeType $EmployeeType -StartDate -otherValues $OtherValues
+    New-FreshServiceOffboardingRequest -EmployeeEmail employee@test.com -ManagerEmail Manager@test.com -otherValues $OtherValues
 
     id            : 9
     created_at    : 2025-02-06T22:04:05Z
@@ -44,10 +40,8 @@
 function New-FreshServiceOffboardingRequest {
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
     param (
-        [Parameter(Mandatory = $true)][string]$EmployeeType,
-        [Parameter(Mandatory = $true)][string]$FirstName,
-        [Parameter(Mandatory = $true)][string]$LastName,
-        [Parameter(Mandatory = $true)][string]$StartDate,
+        [Parameter(Mandatory = $true)][string]$EmployeeEmail,
+        [Parameter(Mandatory = $true)][string]$ManagerEmail,
         [Parameter(Mandatory = $false)][hashtable]$OtherValues
     )
     $PrivateData = $MyInvocation.MyCommand.Module.PrivateData
@@ -58,10 +52,8 @@ function New-FreshServiceOffboardingRequest {
     $uri = [System.UriBuilder]('{0}/offboarding_requests' -f $PrivateData['FreshserviceBaseUri'])
     $requestBody = @{
         fields = @{
-            cf_employee_type                = $EmployeeType
-            cf_first_name                   = $FirstName
-            cf_new_hire_first_and_last_name = $LastName
-            cf_date_of_joining              = $StartDate
+            cf_employee_email = $EmployeeEmail
+            cf_direct_manager = $ManagerEmail
         }
     }
 
@@ -85,10 +77,10 @@ function New-FreshServiceOffboardingRequest {
                 #When using Filter, the API also returns a Total property, so we are filtering here to only return ticket or tickets property
                 $objProperty = $content[0].PSObject.Properties.Name
                 Write-Verbose -Message ("Returning {0} property with count {1}" -f $objProperty, $content."$($objProperty)".Count)
-                $OnboardRequest = $content."$($objProperty)"
-                Write-Verbose -Message ("Created Offboarding Request with ID {0}" -f $OnboardRequest.id)
-                Write-Verbose -Message ("Getting Offboarding Request {0} Parent Ticket" -f $OnboardRequest.id)
-                $ParentTicket = get-freshserviceoffboardingrequest -id $OnboardRequest.id -tickets -ErrorAction Stop
+                $offboardRequest = $content."$($objProperty)"
+                Write-Verbose -Message ("Created Offboarding Request with ID {0}" -f $offboardRequest.id)
+                Write-Verbose -Message ("Getting Offboarding Request {0} Parent Ticket" -f $offboardRequest.id)
+                $ParentTicket = Get-FreshServiceOffboardingRequest -id $offboardRequest.id -tickets -ErrorAction Stop
                 $ParentTicket = $ParentTicket | Where-Object parent -EQ $true
                 Write-Verbose -Message ("Found Parent Ticket {0}" -f $ParentTicket.id)
                 $ParentTicket.id
